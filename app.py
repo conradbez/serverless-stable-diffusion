4811b4f41d35a57f10c0a878853a834d7eef00f7
Conrad <conradbez1@gmail.com>
2023-06-23 23:07:25 +0800

    first commit

--- app.py
+++ app.py
@@ -1,33 +1,49 @@
-from potassium import Potassium, Request, Response
-
-from transformers import pipeline
 import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
+import base64
+from io import BytesIO
+import os
 
-app = Potassium("my_app")
-
-# @app.init runs at startup, and loads models into the app's context
-@app.init
+# Init is ran on server startup
+# Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-   
-    context = {
-        "model": model
-    }
+    global model
+    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
+    
+    # this will substitute the default PNDM scheduler for K-LMS  
+    lms = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
 
-    return context
+    model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=lms, use_auth_token=HF_AUTH_TOKEN)
 
-# @app.handler runs for every call
-@app.handler()
-def handler(context: dict, request: Request) -> Response:
-    prompt = request.json.get("prompt")
-    model = context.get("model")
-    outputs = model(prompt)
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model_inputs:dict) -> dict:
+    global model
 
-    return Response(
-        json = {"outputs": outputs[0]}, 
-        status=200
-    )
+    # Parse out your arguments
+    prompt = model_inputs.get('prompt', None)
+    height = model_inputs.get('height', 512)
+    width = model_inputs.get('width', 512)
+    num_inference_steps = model_inputs.get('num_inference_steps', 50)
+    guidance_scale = model_inputs.get('guidance_scale', 7.5)
+    input_seed = model_inputs.get("seed",None)
+    
+    #If "seed" is not sent, we won't specify a seed in the call
+    generator = None
+    if input_seed != None:
+        generator = torch.Generator("cuda").manual_seed(input_seed)
+    
+    if prompt == None:
+        return {'message': "No prompt provided"}
+    
+    # Run the model
+    with autocast("cuda"):
+        image = model(prompt,height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator)["sample"][0]
+    
+    buffered = BytesIO()
+    image.save(buffered,format="JPEG")
+    image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-if __name__ == "__main__":
-    app.serve()
+    # Return the results as a dictionary
+    return {'image_base64': image_base64}
--- download.py
+++ download.py
@@ -6,6 +6,16 @@ from transformers import pipeline
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     pipeline('fill-mask', model='bert-base-uncased')
+    
+    # from diffusers import StableDiffusionPipeline
+    # import torch
+    # model_id = "prompthero/openjourney"
+    # pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    # pipe = pipe.to("cuda")
+    # prompt = "retro serie of different cars with different colors and shapes, mdjrny-v4 style"
+    # image = pipe(prompt).images[0]
+    # image.save("./retro_cars.png")
 
 if __name__ == "__main__":
     download_model()
+    
